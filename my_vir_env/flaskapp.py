d43b5e24a607cfbc638da718ed6cdf520c68c4a4
Raj Joshi <rajjoshi@Rajs-MacBook-Pro.local>
2022-01-19 15:53:44 +0800

    flask progress so far

--- my_vir_env/flaskapp.py
+++ my_vir_env/flaskapp.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
+posts =[
+    {
+        "title":'Blog Post 1',
+       "author":'Andrew Ng',
+       "date":'Jan 15, 2022'
+       }
+    ]
+
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
